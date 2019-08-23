c875d5117f8e1e5317fe1d5fc8fb5dfe24fbc3d4
Derril Lucci <derril.lucci@gmail.com>
2019-08-23 15:15:29 -0400

    Adding dependencies for my project

--- app/src/main/java/com/dlucci/weather/MainActivity.kt
+++ app/src/main/java/com/dlucci/weather/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.dlucci.weather
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
