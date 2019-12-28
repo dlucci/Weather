af74d38cc996690f6928b52b4f7c1085f60e6e39
Derril Lucci <derril.lucci@gmail.com>
2019-12-28 17:59:56 -0500

    Migrated to koin 2.0.1

---
+++ app/src/main/java/com/dlucci/weather/forecast/di/AppModule.kt
@@ -0,0 +1,24 @@
+package com.dlucci.weather.forecast.di
+
+import com.dlucci.weather.forecast.networking.ForecastService
+import org.koin.core.qualifier.named
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+val networkModule = module {
+    single<Retrofit>(named(DEFAULT_NAMESPACE)) { createRetrofit() }
+    single { createService(get(named(DEFAULT_NAMESPACE))) }
+}
+
+fun createService(retrofit: Retrofit) = retrofit.create(ForecastService::class.java)
+
+fun createRetrofit() = Retrofit.Builder()
+    .baseUrl("https://api.openweathermap.org/data/2.5/")
+    .addConverterFactory(GsonConverterFactory.create())
+    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+    .build()
+
+
+const val DEFAULT_NAMESPACE = "weather"
